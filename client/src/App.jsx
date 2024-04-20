1b200f1caa2dec4c1e370d0adaef7665584331c3
Subrata Das <subratadas3250@gmail.com>
2024-04-20 23:49:37 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Downloader from './components/Downloader';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 function App() {
     return (
         <div className='container'>
-            <Navbar />
+            {/* <Navbar /> */}
             <Downloader />
             <Footer />
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Downloader from './components/Downloader';
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Downloader from './components/Downloader';
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        // loader: rootLoader,
-        children: [
-            {
-                path: "/:videoId",
-                element: <Downloader />,
-            },
-        ],
-    },
-]);
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <App />,
+//         // loader: rootLoader,
+//         children: [
+//             {
+//                 path: "/:videoId",
+//                 element: <Downloader />,
+//             },
+//         ],
+//     },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
